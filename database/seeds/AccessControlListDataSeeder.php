e1aed6350c016b7ac9e01c8492df03602fa3c870
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2019-02-09 22:40:17 +0500

    updated acl seeder

--- database/seeds/AccessControlListDataSeeder.php
+++ database/seeds/AccessControlListDataSeeder.php
@@ -25,12 +25,98 @@ class AccessControlListDataSeeder extends Seeder
         // define base permissions for each role
         $permissions = [
             "super-admin" => [
-                "create-admin" => "Can create School Administrators"
+                "update-self" => "Can update own profile",
+                // school admins
+                "create-admin" => "Can create School Administrators",
+                "update-admin" => "Can update School Administrators",
+                "delete-admin" => "Can delete School Administrators",
+                "approve-admin" => "Can approve School Administrators",
+                "read-admin" => "Can see and search School Administrators",
+                // categories
+                "create-category" => "Can create Categories",
+                "update-category" => "Can update Categories",
+                "delete-category" => "Can delete Categories",
+                "read-category" => "Can see and search Categories",
+                // courses
+                "create-course" => "Can create Courses",
+                "update-course" => "Can update Courses",
+                "delete-course" => "Can delete Courses",
+                "read-course" => "Can see and search Courses",
+                // territories
+                "create-territory" => "Can create Territories",
+                "update-territory" => "Can update Territories",
+                "delete-territory" => "Can delete Territories",
+                "read-territory" => "Can see and search Territories",
+                // state regulations
+                "create-state" => "Can create State Regulations",
+                "update-state" => "Can update State Regulations",
+                "delete-state" => "Can delete State Regulations",
+                "read-state" => "Can see and search State Regulations"
+            ],
+            "admin" => [
+                "update-self" => "Can update own profile",
+                // classes
+                "create-class" => "Can create Classes",
+                "update-class" => "Can update Classes",
+                "delete-class" => "Can delete Classes",
+                "approve-class" => "Can approve Classes",
+                "cancel-class" => "Can cancel Classes",
+                "archive-class" => "Can archive Classes",
+                "read-class" => "Can see and search Classes",
+                // sponsors
+                "create-sponsor" => "Can create Sponsors",
+                "update-sponsor" => "Can update Sponsors",
+                "delete-sponsor" => "Can delete Sponsors",
+                "read-sponsor" => "Can see and search Sponsors",
+                // instructors
+                "create-instructor" => "Can create Instructors",
+                "update-instructor" => "Can update Instructors",
+                "delete-instructor" => "Can delete Instructors",
+                "read-instructor" => "Can see and search Instructors",
+                "approve-instructor" => "Can approve Instructors",
+                // venues
+                "create-venue" => "Can create Venues",
+                "update-venue" => "Can update Venues",
+                "delete-venue" => "Can delete Venues",
+                "read-venue" => "Can see and search Venues"
+            ],
+            "instructor" => [
+                "update-self" => "Can update own profile",
+                // classes
+                "create-class" => "Can create Classes",
+                "enroll-in-class" => "Can enroll Students in his Class",
+                "archive-class" => "Can archive Classes",
+                "cancel-class" => "Can cancel Classes",
+                // sponsors
+                "create-sponsor" => "Can create Sponsors",
+                "update-sponsor" => "Can update Sponsors",
+                "delete-sponsor" => "Can delete Sponsors",
+                "read-sponsor" => "Can see and search Sponsors",
+                // venues
+                "create-venue" => "Can create Venues",
+                "update-venue" => "Can update Venues",
+                "delete-venue" => "Can delete Venues",
+                "read-venue" => "Can see and search Venues",
+                // courses
+                "read-course-material" => "Can see and Course Materials"
+            ],
+            "student" => [
+                "update-self" => "Can update own profile",
+                "read-class" => "Can see his Classes",
             ]
         ];
 
-        // $role->assign($permission)
+        // assign permissions to roles
+        foreach ($roles as $role) {
+            $role_permissions = $permissions[$role];
+            foreach ($role_permissions as $permission) {
+                $role->assign($permission);
+            }
+        }
+
         $user = User::find(1);
-        // $user->assign($roles["super-admin"])
+        $user->assign("super-admin");
+        $user->assign("admin");
+        $user->assign("instructor");
     }
 }
