34f98f9c73067b9387a1d7c6078b93aa6a6378a7
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2019-02-26 22:47:36 +0500

    backend: territories

--- app/Http/Controllers/TerritoriesController.php
+++ app/Http/Controllers/TerritoriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Territory;
 use Illuminate\Http\Request;
 
 class TerritoriesController extends Controller
@@ -13,7 +14,10 @@ class TerritoriesController extends Controller
      */
     public function index()
     {
-        //
+        $territories = Territory::with('regulation')->get();
+        return response()->json([
+            'territories' => $territories
+        ], 200);
     }
 
     /**
@@ -24,40 +28,60 @@ class TerritoriesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $territory = Territory::create([
+            'name' => $request->get('name'),
+            'regulation_id' => $request->get('regulation'),
+            'zip_codes' => $request->get('zip_codes')
+        ]);
+        return response()->json([
+            'territory' => $territory
+        ], 200);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Territory $territory
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Territory $territory)
     {
-        //
+        return response()->json([
+            'territory' => $territory
+        ], 200);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Territory $territory
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Territory $territory)
     {
-        //
+        $territory->update([
+            'name' => $request->get('name'),
+            'regulation_id' => $request->get('regulation'),
+            'zip_codes' => $request->get('zip_codes')
+        ]);
+        return response()->json([
+            'territory' => $territory
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Territory $territory
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Territory $territory)
     {
-        //
+        $territory->delete();
+        return response()->json([
+            'territory' => 'success'
+        ], 200);
     }
 }
--- app/Territory.php
+++ app/Territory.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Territory extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'regulation_id', 'zip_codes'
+    ];
+
     public function regulation()
     {
         return $this->belongsTo(Regulation::class);
