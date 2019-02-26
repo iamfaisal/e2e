1b64c6bcc9f3eea3112a3efa079141f8885eabce
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2019-02-26 23:18:21 +0500

    backend: regulations

--- app/Regulation.php
+++ app/Regulation.php
@@ -6,8 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Regulation extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'abbreviation', 'commission_name', 'commission_abbreviation',
+        'contact_first_name', 'contact_last_name', 'contact_email_address', 'contact_phone',
+        'contact_street_address', 'contact_city', 'contact_state', 'contact_zip_code',
+        'regulations', 'regulations_doc', 'ce_requirements_statement', 'must_specify_courses'
+    ];
+
     public function courses()
     {
         return $this->hasMany(Course::class);
+    }
+
+    public function territories()
+    {
+        return $this->hasMany(Territory::class);
     }
 }
