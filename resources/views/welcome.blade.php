94814f55a16204f8c89b3886ca0f0c81309b8359
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2019-03-02 00:57:52 +0500

    [BE] added favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,9 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{asset('images/icon.png')}}" sizes="192x192" />
+        <link rel="apple-touch-icon-precomposed" href="{{asset('images/icon.png')}}" />
+        <meta name="msapplication-TileImage" content="{{asset('images/icon.png')}}" />
         <title>MyCE App</title>
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
