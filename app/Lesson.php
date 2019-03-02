2658b24aaa9eb02ca1d8ce2aee8d94486510e875
Faisal Mughal <faisal.mughal.iefr@gmail.com>
2019-03-03 01:18:57 +0500

    [BE] migration Lessons: classes

--- app/Lesson.php
+++ app/Lesson.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lesson extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'course_id', 'venue_id', 'start_date', 'end_date', 'price', 'capacity',
+        'alternate_instructor', 'guest_speaker', 'rsvp_contact', 'rsvp_phone', 'rsvp_email',
+        'rsvp_link_text', 'rsvp_link_url', 'flyer', 'flyer_image', 'docs', 'roster', 'status',
+        'is_approved', 'is_deleted', 'is_cancelled'
+    ];
 }
--- database/migrations/2019_03_02_190542_create_lessons_table.php
+++ database/migrations/2019_03_02_190542_create_lessons_table.php
@@ -15,6 +15,31 @@ class CreateLessonsTable extends Migration
     {
         Schema::create('lessons', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned()->default(1);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('course_id')->unsigned()->default(1);
+            $table->foreign('course_id')->references('id')->on('courses');
+            $table->integer('venue_id')->unsigned()->default(1);
+            $table->foreign('venue_id')->references('id')->on('venues');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('price');
+            $table->integer('capacity');
+            $table->string('alternate_instructor')->nullable();
+            $table->string('guest_speaker')->nullable();
+            $table->string('rsvp_contact')->nullable();
+            $table->string('rsvp_phone')->nullable();
+            $table->string('rsvp_email')->nullable();
+            $table->string('rsvp_link_text')->nullable();
+            $table->string('rsvp_link_url')->nullable();
+            $table->string('flyer')->nullable();
+            $table->string('flyer_image')->nullable();
+            $table->string('docs')->nullable();
+            $table->string('roster')->nullable();
+            $table->string('status');
+            $table->boolean('is_approved')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->boolean('is_cancelled')->default(0);
             $table->timestamps();
         });
     }
