81197b704951d5763a327091a013ffdea3146185
iamfaisal <faisal.mughal.iefr@gmail.com>
2019-03-29 23:14:33 +0500

    [BE] Classes (edit + delete)

--- app/Http/Controllers/ClassesController.php
+++ app/Http/Controllers/ClassesController.php
@@ -68,7 +68,9 @@ class ClassesController extends Controller
      */
     public function show(Lesson $class)
     {
-        //
+        return response()->json([
+            'class' => $class
+        ], 200);
     }
 
     /**
@@ -80,7 +82,27 @@ class ClassesController extends Controller
      */
     public function update(Request $request, Lesson $class)
     {
-        //
+        $data = [
+            'user_id' => $request->get('instructor'),
+            'course_id' => $request->get('course'),
+            'venue_id' => $request->get('venue'),
+            'start_date' => $request->get('start_date_time'),
+            'end_date' => $request->get('end_date_time'),
+            'price' => $request->get('price'),
+            'capacity' => $request->get('capacity'),
+            'alternate_instructor' => $request->get('alternate_instructor'),
+            'guest_speaker' => $request->get('guest_speaker'),
+            'rsvp_contact' => $request->get('rsvp_contact'),
+            'rsvp_phone' => $request->get('rsvp_phone'),
+            'rsvp_email' => $request->get('rsvp_email'),
+            'rsvp_link_text' => $request->get('rsvp_link_text'),
+            'rsvp_link_url' => $request->get('rsvp_link_url'),
+            'status' => 'New'
+        ];
+        $class->update($data);
+        return response()->json([
+            'class' => $class
+        ], 200);
     }
 
     /**
@@ -88,9 +110,13 @@ class ClassesController extends Controller
      *
      * @param  Lesson $class
      * @return \Illuminate\Http\Response
+     * @throws \Exception
      */
     public function destroy(Lesson $class)
     {
-        //
+        $class->delete();
+        return response()->json([
+            'class' => 'success'
+        ], 200);
     }
 }
