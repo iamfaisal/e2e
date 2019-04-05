957829d78160d3a7de6e4d6704421f6ed10dc5aa
iamfaisal <faisal.mughal.iefr@gmail.com>
2019-04-05 22:25:03 +0500

    [BE] email notifications: class created

--- resources/views/mail/class/created.blade.php
+++ resources/views/mail/class/created.blade.php
@@ -1,11 +1,21 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+    **This message does not constitute final class approval. It’s important
+    you do not begin marketing the class until you receive the final CE
+    class approval email.**
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
+    Your CE class request has been received and will be reviewed by the E2E Team within 2 business days.
+
+    @component('mail::table')
+        | ------------- |:------------------------:|
+        | CE Course Code| {{ $data->course_code }} |
+        | CE Class Date | {{ $data->course_code }} |
+        | Instructor    | {{ $data->course_code }} |
+    @endcomponent
+
+    Please contact the E2E Team at
+    [educate2earn@amerifirst.us](mailto:educate2earn@amerifirst.us) with any
+    questions.
 
 Thanks,<br>
 {{ config('app.name') }}
